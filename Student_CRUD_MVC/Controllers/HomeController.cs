68d3aa79e56ec07f19b63e6aeda5ad0081450b5f
SnehalS26 <Snehalshirsath26@gmail.com>
2023-09-02 15:51:36 +0530

    chnage

--- Student_CRUD_MVC/Controllers/HomeController.cs
+++ Student_CRUD_MVC/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Student_CRUD_MVC.Models;
 using System.Diagnostics;
 
@@ -15,7 +16,23 @@ namespace Student_CRUD_MVC.Controllers
 
         public IActionResult Index()
         {
+            List<string> cities = new List<string>() { "Pune", "Mumbai", "Nashik", "Nagpur" };
+           //single list= single selection, iteration
+           //multiselect list = multiple selection , iteration
+            ViewData["cities"] = new SelectList(cities);
+            ViewData["list"] = new MultiSelectList(cities);
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(IFormCollection form , ICollection<string> hobbies)
+        {
+            ViewBag.Username = form["username"];
+            ViewBag.Gender = form["gender"];
+            ViewBag.City = form["cities"];
+            ViewBag.Hobbies = hobbies;
+            ViewBag.Comments = form["comments"];
+            return View("ShowInfo");
         }
 
         public IActionResult Privacy()
